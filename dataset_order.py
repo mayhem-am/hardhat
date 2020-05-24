282a8bffd5e11e4cc0615c88e432904c638db787
Akanksh A Manjunath <amakanksh@gmail.com>
2020-05-24 17:33:47 +0530

    Add files via upload

---
+++ dataset_order.py
@@ -0,0 +1,21 @@
+import os 
+  
+# Function to rename multiple files 
+def main(): 
+    i = 1   # or set to appropriate start 
+      
+    for filename in os.listdir("Helmet"): 
+        dst = str(i) + ".jpg"
+        src = 'Helmet/'+ filename 
+        dst ='Helmet/'+ dst 
+          
+        # rename() function will 
+        # rename all the files 
+        os.rename(src, dst) 
+        i += 1
+  
+# Driver Code
+if __name__ == '__main__': 
+      
+    # Calling main() function 
+    main()
